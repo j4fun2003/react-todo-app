0f81291010b79f90fbff46e67b91aa620314b269
Thai Huy <j4fun2003@gmail.com>
2024-03-24 20:27:23 +0700

    refactor es6 and remove editing variable

--- src/components/Header1.js
+++ src/components/Header1.js
@@ -3,30 +3,26 @@ import React from 'react';
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: ''
-    };
   }
 
   handleOnkey = (event) => {
     if (event.key === "Enter") {
-      const value = this.state.value;
-      this.props.addItem(value);
-      this.setState({ value: '' });
+      this.props.addItem(event.target.value);
+      event.target.value="";
     }
   };
 
-  handleOnChange = (event) => {
-    const { value } = event.target;
-    this.setState({ value });
-  };
+  // handleOnChange = (event) => {
+  //   const { value } = event.target;
+  //   this.setState({ value });
+  // };
 
   render() {
     return (
       <header>
         <h1 class="title">todos</h1>
         <div class="input-area">
-          <input type="text" value={this.state.value} className="input-text" placeholder="What needs to be done?" onChange={this.handleOnChange} onKeyDown={this.handleOnkey}></input>
+          <input type="text" className="input-text" placeholder="What needs to be done?"  onKeyDown={this.handleOnkey}></input>
         </div>
       </header>
     );
